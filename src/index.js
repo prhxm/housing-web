584cbddb719ef50d50a7ec9ef95f87824e32ad72
prhxm <parhamforfun11@gmail.com>
2025-06-02 20:38:19 -0700

    fix: switch to frontendApi instead of clerkJSUrl

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import "./index.css";
 
 
 const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
-const clerkJSUrl = process.env.REACT_APP_CLERK_FRONTEND_API;
+const clerkFrontendApi = process.env.REACT_APP_CLERK_FRONTEND_API;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey} clerkJSUrl={clerkJSUrl}>
+    <ClerkProvider publishableKey={clerkPubKey} frontendApi ={clerkFrontendApi}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
